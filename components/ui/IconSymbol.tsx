cab7d16af12122560107f9a0e25f71bb50df4da9
Bartłomiej Skorupski <bartmann001@gmail.com>
2025-04-25 21:43:19 +0200

    fun fact

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'clock.circle.fill': 'cached',
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
